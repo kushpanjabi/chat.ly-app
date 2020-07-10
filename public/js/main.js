03cfbc6d80a543ed59a2c01d16cffe5039a24df7
Kush Panjabi <kushpanjabi@kushs-mbp.lan>
2020-07-10 07:52:26 -0700

    added socket username and room functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,50 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+
+// Get username and room from URL
+const { username, room } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+});
+
+console.log(username, room);
+
 const socket = io();
 
+// Join a chatroom
+socket.emit('joinRoom', { username, room })
+
+// Message from server
 socket.on('message', message => {
     console.log(message);
+    outputMessage(message);
+
+    // Auto scroll down when receiving a new message not in view
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 });
+
+// Message submit
+
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Getting the chat message text
+    const msg = e.target.elements.msg.value;
+
+    // Emit chat message text to server
+    socket.emit('chatMessage', msg);
+
+        // Clear input
+        e.target.elements.msg.value = '';
+        e.target.elements.msg.focus();
+});
+
+// Output message to DOM
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+    <p class="text">
+        ${message.text}
+    </p>`;
+     document.querySelector('.chat-messages').appendChild(div);
+}
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser } = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,11 +12,31 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'chat.ly bot'
+
 // Run when a client connects
 io.on('connection', socket => {
-    console.log('New Web Socket Connection...');
+        socket.on('joinRoom', ({ username, room }) => {
 
-    socket.emit('message', 'Welcome to Chat.ly');
+            const user = userJoin(socket.id, username, room);
+            socket.join(user.room);
+
+        // Welcome current user
+        socket.emit('message', formatMessage(botName, 'Welcome to Chat.ly'));
+
+        // Broadcast when a user connects
+        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`));
+    });
+
+    // Listen for chatMessage
+    socket.on('chatMessage', (msg) => {
+        io.emit('message', formatMessage('USER', msg));
+    });
+
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', formatMessage(botName,'A user has left the chat'));
+    });
 });
 
 const PORT = process.env.PORT || 3000;
