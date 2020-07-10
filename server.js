af05061470391963580ca9bbf26665a5bb2f374e
Kush Panjabi <kushpanjabi@kushs-mbp.lan>
2020-07-09 20:52:54 -0700

    created starter express server and static html content

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when a client connects
+io.on('connection', socket => {
+    console.log('New Web Socket Connection...');
+
+    socket.emit('message', 'Welcome to Chat.ly');
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
